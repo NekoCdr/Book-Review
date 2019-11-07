769c2d81befc1afe3fb62f7a7799b887b1d4e518
Yuri Istomin <intro1994@gmail.com>
2019-11-07 19:01:39 +0200

    Added 'EntryPoints'
    - Added 'EntryPoint' model & migration

---
+++ database/migrations/2019_11_06_092939_create_entry_points_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntryPointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entry_points', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->integer('user_id');
+            $table->smallInteger('type');
+            $table->string('password');
+            $table->string('login_name');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entry_points');
+    }
+}
