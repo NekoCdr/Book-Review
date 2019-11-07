9cfd6a667ba1ea3b03c79059f9fee7af0c4391e3
Yuri Istomin <intro1994@gmail.com>
2019-11-07 19:01:18 +0200

    Added 'Comments'
    - Added 'Comment' model & migration

---
+++ database/migrations/2019_11_06_092921_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->integer('user_id');
+            $table->integer('book_id');
+            $table->text('comment');
+            $table->integer('parent_comment');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
